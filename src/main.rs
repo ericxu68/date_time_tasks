3aef3b0576a4389d4a503807c84e4c08ddac5bd8
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-10 10:56:45 +0000

    Get date three weeks from now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use chrono::{DateTime, Duration, Utc};
+use std::thread;
+use std::time::{Duration as duree, Instant};
+
+fn expensive_function() {
+    thread::sleep(duree::from_secs(1));
+}
+
+fn day_earlier(date_time: DateTime<Utc>) -> Option<DateTime<Utc>> {
+    date_time.checked_sub_signed(Duration::days(1))
+}
+
 fn main() {
-    println!("Hello, world!");
+    // time ones code
+    let start = Instant::now();
+    expensive_function();
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
+    let now = Utc::now();
+    println!("{}", now);
+
+    let three_weeks_from_now = now
+        .checked_add_signed(Duration::weeks(2))
+        .and_then(|two_weeks| two_weeks.checked_add_signed(Duration::weeks(1)))
+        .and_then(day_earlier);
+    // use match to unpack the Option
+    match three_weeks_from_now {
+        Some(three_weeks_from_now) => {
+            println!("{}", three_weeks_from_now);
+        }
+        None => {
+            eprint!("Almost three weeks from now overflows!");
+        }
+    }
+    // println!("{}", three_weeks_from_now.unwrap());
+
+    match now.checked_add_signed(Duration::max_value()) {
+        Some(x) => println!("{}", x),
+        None => eprintln!("We can't use chrono to tell the time for the Solar System to complete more than one full orbit around the galactic center."),
+    }
 }
