d99d768dd84b6b5b494f20ba5fcc984b8933993d
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-10 11:08:39 +0000

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+1. time's code
+2. GET DATE THREE WEEKS FROM NOW
+3. CONVERT LOCAL TIMEZONE TO ANOTHER
+
+TODO: 
+---
+- [ ] Make a Timezone clock
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Duration, Utc};
+use chrono::{DateTime, Duration, FixedOffset, Local, NaiveDate, Utc};
 use std::thread;
 use std::time::{Duration as duree, Instant};
 
@@ -11,11 +11,18 @@ fn day_earlier(date_time: DateTime<Utc>) -> Option<DateTime<Utc>> {
 }
 
 fn main() {
+    /******************
+     * TIME ONE'S CODE
+     ******************/
     // time ones code
     let start = Instant::now();
     expensive_function();
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
+
+    /*******************************
+     * GET DATE THREE WEEKS FROM NOW
+     *******************************/
 
     let now = Utc::now();
     println!("{}", now);
@@ -39,4 +46,26 @@ fn main() {
         Some(x) => println!("{}", x),
         None => eprintln!("We can't use chrono to tell the time for the Solar System to complete more than one full orbit around the galactic center."),
     }
+
+    /*************************************
+     * CONVERT LOCAL TIMEZONE TO ANOTHER
+     *************************************/
+
+    let local_time = Local::now();
+    let utc_time = DateTime::<Utc>::from_utc(local_time.naive_utc(), Utc);
+    let timezone_east = FixedOffset::east_opt(8 * 60 * 60).unwrap();
+    let naivedatetime_east = NaiveDate::from_ymd_opt(2000, 1, 12)
+        .unwrap()
+        .and_hms_opt(10, 0, 0)
+        .unwrap();
+    let datetime_east = DateTime::<FixedOffset>::from_local(naivedatetime_east, timezone_east);
+    println!("Eastern time is: {}", datetime_east);
+
+    let timezone_west = FixedOffset::west_opt(7 * 60 * 60).unwrap();
+    let naivedatetime_west = NaiveDate::from_ymd_opt(2000, 1, 11)
+        .unwrap()
+        .and_hms_opt(19, 0, 0)
+        .unwrap();
+    let datetime_west = DateTime::<FixedOffset>::from_local(naivedatetime_west, timezone_west);
+    println!("Western time is: {}", datetime_west);
 }
